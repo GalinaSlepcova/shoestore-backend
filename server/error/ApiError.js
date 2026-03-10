dc16b7d34506efe4e8fdcb6106d32caf34586c5f
Galina Slepcova <galina20040323@gmail.com>
2026-03-10 15:19:42 +0300

    middleware

--- server/error/ApiError.js
+++ server/error/ApiError.js
@@ -1,16 +1,23 @@
 class ApiError extends Error {
     constructor(status, message) {
         super();
-        this.status = status
-        this.message = message
+        this.status = status;
+        this.message = message;
     }
 }
 
 
-static badRequest(message) {
+ApiError.badRequest = (message) => {
     return new ApiError(404, message)
 }
 
-static internal(message) {
+ApiError.internal = (message) => {
     return new ApiError(500, message)
 }
+
+ApiError.forbidden = (message) => {
+    return new ApiError(403, message)
+}
+
+
+module.exports = ApiError
